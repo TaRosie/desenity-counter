7f353b6453bfa585e93f87890b686cd08e572406
TaRosie <hg1322@nyu.edu>
2019-12-04 11:12:54 +0800

    shop

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-// import ReactGA from 'react-ga';
 
-// function initializeReactGA(){
-// ReactGA.initialize('new');
-// ReactGA.pageview("/");
-// }
-
-// initializeReactGA();
-
-
-let lounge = [
-  {name:'8F sofa',
+let shoppingCartItems = [
+  {
+    item: '8F',
+    description: 'This is the 8F lounge.',
+    density: 500,
   },
   {
-    name: '8F block',
-  }
+    item: '3F',
+    description: 'This is the 3F lounge',
+    density: 150,
+  },
+  {
+    item: '2F',
+    description: 'This is the 2F lounge',
+   density: 100, 
+  },
+  {
+    item: '3F',
+    description: 'This is the 3F lounge',
+    density: 150,
+  },
+
 ]
 
 function App() {
-
-  // const[mode, setMode] =  useState('crowded');
-
-  // function handleClick(){
-  //   console.log('clicked');
-  //   }
-  //   ReactGA.event({
-  //     category:'Interaction',
-  //     action:'Change the mode'
-  //   });
-  // }
-
-
-  // let imgPath = (mode === 'Personal' ? article[0].image : article[1].image);
-
   return (
     <div className="App">
-      <h1>NYU Shanghai floor map</h1>
-      <h2>Check for available lounges</h2>  
-{/*  
-      <button onClick={handleClick}> Click to see functions </button> */}
-  </div>
+      <h1>NYU Shanghai Floor Map</h1>
+      <h2>Check for available lounges!</h2>
+     <ShoppingCart />
+    </div>
   );
 }
 
+function makeProduct(singleProduct){
+return(
+  <Product item={singleProduct.item} image={singleProduct.image} description={singleProduct.description} density={singleProduct.density}/>
+)
+}
+
+function ShoppingCart() {
+  const products = shoppingCartItems.map(makeProduct);
+  return(
+    <div className="cart">
+    {products}
+    </div>
+  );
+}
+
+function Product(props) {
+  return(
+    <div className="product">
+      <p className="product-title">{props.item}</p>
+      <p className="product-description">{props.description}</p>
+      <p className="product-price">{props.density}</p>
+      
+
+    </div>
+  )
+}
+
+
 
 export default App;
-
